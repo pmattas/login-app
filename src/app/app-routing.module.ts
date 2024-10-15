9da7e5e252ede9b48b9e44de35791d6035aec2f3
pmattas <pmattas@extremenetworks.com>
2024-10-15 14:58:05 +0530

    Correction for default path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ForgotpwdComponent } from './forgotpwd/forgotpwd.component';
 import { SecurityComponent } from './security/security.component';
 
 const routes: Routes = [
-  { path:'', component: LoginComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent},
   { path: 'security', component: SecurityComponent},
